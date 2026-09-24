e20211769aea92538d5270a7ea892870cfbc482f
ngtonnn <nguyenngoctoan14112007@gmail.com>
2026-09-24 14:39:17 +0700

    Fix respring button

--- hardkeyfixprefs/HKFRootListController.m
+++ hardkeyfixprefs/HKFRootListController.m
@@ -1,19 +1,24 @@
+#import <Foundation/Foundation.h>
 #import "HKFRootListController.h"
-#import <spawn.h>
+#include <spawn.h>
 
 @implementation HKFRootListController
 
 - (NSArray *)specifiers {
-	if (!_specifiers) {
-		_specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
-	}
-	return _specifiers;
+    if (!_specifiers) {
+        _specifiers = [self loadSpecifiersFromPlistName:"Root" target:self];
+    }
+    return _specifiers;
 }
 
 - (void)respring {
     pid_t pid;
-    const char *args[] = {"killall", "-9", "SpringBoard", NULL};
-    posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char *const *)args, NULL);
+    const char* args[] = {"killall", "-9", "SpringBoard", NULL};
+    if (access("/var/jb/usr/bin/killall", F_OK) == 0) {
+        posix_spawn(&pid, "/var/jb/usr/bin/killall", NULL, NULL, (char* const*)args, NULL);
+    } else if (access("/usr/bin/killall", F_OK) == 0) {
+        posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char* const*)args, NULL);
+    }
 }
 
 @end
