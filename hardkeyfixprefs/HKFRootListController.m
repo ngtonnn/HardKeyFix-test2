8ff1bab3c3086cf939336a6b2d0595248c18b01d
ngtonnn <nguyenngoctoan14112007@gmail.com>
2026-09-24 14:59:16 +0700

    Remove respring button

--- hardkeyfixprefs/HKFRootListController.m
+++ hardkeyfixprefs/HKFRootListController.m
@@ -1,6 +1,5 @@
 #import <Foundation/Foundation.h>
 #import "HKFRootListController.h"
-#include <spawn.h>
 
 @implementation HKFRootListController
 
@@ -9,16 +8,6 @@
         _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
     }
     return _specifiers;
-}
-
-- (void)respring {
-    pid_t pid;
-    const char* args[] = {"killall", "-9", "SpringBoard", NULL};
-    if (access("/var/jb/usr/bin/killall", F_OK) == 0) {
-        posix_spawn(&pid, "/var/jb/usr/bin/killall", NULL, NULL, (char* const*)args, NULL);
-    } else if (access("/usr/bin/killall", F_OK) == 0) {
-        posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char* const*)args, NULL);
-    }
 }
 
 @end
